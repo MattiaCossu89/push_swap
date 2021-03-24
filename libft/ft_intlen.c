aa9c4a64460a202e1a7850fe9425ef48450ef49d
Mattia Cossu <mcossu@e3r10p7.42roma.it>
2021-03-24 18:34:55 +0100

    intlen

--- libft/ft_intlen.c
+++ libft/ft_intlen.c
@@ -6,7 +6,7 @@
 /*   By: mcossu <mcossu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 11:52:07 by mcossu            #+#    #+#             */
-/*   Updated: 2021/03/24 18:33:51 by mcossu           ###   ########.fr       */
+/*   Updated: 2021/03/24 18:34:36 by mcossu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,12 @@ int	ft_intlen(int nbr)
 	len = 1;
 	if (nbr < 0)
 	{
-		n = -nbr
+		n = -nbr;
+		len++;
 	}
-	while (nbr /= 10)
+	else
+		n = nbr;
+	while (n /= 10)
 		len++;
 	return (len);
 }
